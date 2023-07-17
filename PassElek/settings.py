0b49de1f47007a4a490fcd7315a38c797873b568
Ali Zareh <80646962+AliZareh-Official@users.noreply.github.com>
2023-07-18 01:23:05 +0300

    Update settings.py

--- PassElek/settings.py
+++ PassElek/settings.py
@@ -23,9 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s)=+50e1n3be%@in1d$rykxgkz^2y)y$i4oig8entb8c%-x2ye'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'server_name',
+    '16.170.248.48',
+    'www.passElektronik.com.tr',
+    'passelektronik.com.tr',
+]
+
 
 # Application definition
 
@@ -118,10 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
-STATICFILES_DIRS = (
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
